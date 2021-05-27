e1328d0151e571438a8efc308efc0caa8e49feda
Kyanka <mozgoprod@gmail.com>
2021-05-27 09:40:13 +0300

    lab4

---
+++ l4/src/module-info.java
@@ -0,0 +1,10 @@
+module l4 {
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires j3dcore;
+    requires vecmath;
+    requires j3dutils;
+}
